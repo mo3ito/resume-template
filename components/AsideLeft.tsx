83b8106fb11cf6febe9435ff0826c662007c24c7
mo3ito <mostafa.tar90@gmail.com>
2024-04-04 22:01:01 +0330

    add color on dark mode and light mode for text

--- components/AsideLeft.tsx
+++ components/AsideLeft.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AsideLeft() {
   return (
-    <aside className="w-3/12 h-full rounded-[30px] border border-zinc-300 bg-[#ebf0f7] dark:bg-[#424550] dark:border-zinc-800 shadow-md">
+    <aside className="w-3/12  rounded-[30px] border border-zinc-300 bg-[#ebf0f7] dark:bg-[#424550] dark:border-zinc-800 shadow-md fixed inset-y-12">
     <h1></h1>
   </aside>
   )
